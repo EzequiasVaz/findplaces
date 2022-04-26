d3e97e53082b2ea14e84c6f91ddf9757f245063f
EzequiasVaz <ezequiasvaz@hotmail.com>
2022-04-26 01:16:32 -0300

    adicionado função ao button de login na pagina search

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,5 @@
 /*movimentando ul do status com os buttons*/
-
+let btnLogin = document.querySelector('.btn-login-search-page')
 let statusList = document.querySelector('.status-list')
 let btnGoLeft = document.querySelector('.arrow-go-left')
 let btnGoRight = document.querySelector('.arrow-go-right')
@@ -7,9 +7,14 @@ let statusWidth = statusList.scrollWidth
 
 
 btnGoRight.addEventListener('click', () => {
-    statusList.scrollLeft -= -100 * 3    
+    statusList.scrollLeft -= -100 * 3
 })
 
 btnGoLeft.addEventListener('click', () => {
-    statusList.scrollLeft -= 100 * 3    
+    statusList.scrollLeft -= 100 * 3
 })
+
+btnLogin.addEventListener('click', () => {
+    window.location.replace('https://ezequiasvaz.github.io/findplaces/login.html')
+})
+
